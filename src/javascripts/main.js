b336f7b19d23140b9a8ab7fb612a6d057c0b4529
Aaron Lain <aaron.j.lain@gmail.com>
2020-03-10 21:18:43 -0500

    Navbar complete

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,15 +1,2 @@
 import '../styles/main.scss';
 import 'bootstrap';
-
-console.error('hi');
-
-const testChecked = () => {
-  $('checkbox').addClass('active');
-  console.error('click');
-};
-
-const clickEvents = () => {
-  $('body').on('click', '#dropdown-item', testChecked);
-};
-
-clickEvents();
