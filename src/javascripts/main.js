33a2e9f29d3d4c21892e1ae2bd686664b62c8256
Stephen Castaneda <stephen.castaneda40@gmail.com>
2020-03-12 14:47:35 -0500

    added large text features

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -3,6 +3,7 @@ import '../styles/main.scss';
 import displayMessage from './components/displayMessage';
 import messageData from './helpers/Data/messageData';
 import darkTheme from './components/darkTheme/darkTheme';
+import largeText from './components/largeText';
 
 console.error('hi');
 
@@ -11,7 +12,12 @@ const init = () => {
   messageData.getUserMessages();
   $('body').on('click', '.delete-btn', displayMessage.deleteMessage);
   darkTheme.turnDark();
-
+  // largeText.largeTextButtonEvent();
+  $(document).ready(() => {
+    $(document).on('click', 'button#large-text', largeText.bigText);
+  });
+  // largeText.makeFontLarge();
+  // largeText.bigText();
 };
 
 init();
