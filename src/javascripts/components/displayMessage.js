795590e02e47f9059a72231897b3da07571ac2c2
Aaron Lain <aaron.j.lain@gmail.com>
2020-03-14 11:43:12 -0500

    WIP

--- src/javascripts/components/displayMessage.js
+++ src/javascripts/components/displayMessage.js
@@ -5,24 +5,27 @@ import messageData from '../helpers/Data/messageData';
 const userMessageCardBuilder = () => {
   const users = messageData.getUserMessages();
   let domString = '';
-  users.forEach((user) => {
-    domString += '<div class="card mb-12">';
-    domString += '<div class="row no-gutters">';
-    domString += '<div class="col-md-4">';
-    domString += `<img src="${user.userImg}" class="card-img" style="max-width: 150px;" alt="Picture of me">`;
-    domString += '</div>';
-    domString += '<div class="col-md-8">';
-    domString += '<div class="card-body">';
-    domString += `<h5 class="card-title">${user.userName}</h5>`;
-    domString += `<p class="card-text">${user.messageContent[0]}</p>`;
-    domString += `<span class=“time-right”>${moment().format('LLL')}</span>`;
-    domString += '</div>';
-    domString += '<div>';
-    domString += `<a href="#" id=${user.id} class="btn btn-danger delete-btn">Delete</a>`;
-    domString += '</div>';
-    domString += '</div>';
-    domString += '</div>';
-  });
+  if (Array.isArray(users.messageContent) && users.messageContent.length === 0) {
+    users.forEach((user) => {
+      domString += '<div class="card mb-12">';
+      domString += '<div class="row no-gutters">';
+      domString += '<div class="col-md-4">';
+      domString += `<img src="${user.userImg}" class="card-img" style="max-width: 150px;" alt="Picture of me">`;
+      domString += '</div>';
+      domString += '<div class="col-md-8">';
+      domString += '<div class="card-body">';
+      domString += `<h5 class="card-title">${user.userName}</h5>`;
+      domString += `<p class="card-text">${user.messageContent[0]}</p>`;
+      domString += `<span class=“time-right”>${moment().format('LLL')}</span>`;
+      domString += '</div>';
+      domString += '<div>';
+      domString += `<a href="#" id=${user.id} class="btn btn-danger delete-btn">Delete</a>`;
+      domString += '</div>';
+      domString += '</div>';
+      domString += '</div>';
+    });
+  }
+
   domString += '</div>';
   utils.printToDom('message-display', domString);
   // messageData.setUserMessages();
@@ -38,7 +41,6 @@ const deleteMessage = (e) => {
 
 const clearAllMessages = (e) => {
   if (e.target.id === 'clear-chat') {
-    console.error(e.target.id);
     $('#message-display').empty();
   }
 };
