4cf29a8f52a9cd4b77a594008e56b0c45f00ab2c
Aaron Lain <aaron.j.lain@gmail.com>
2020-03-15 16:31:57 -0500

    clean-up a smidge

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,7 +1,6 @@
 import 'bootstrap';
 import '../styles/main.scss';
 import displayMessage from './components/displayMessage';
-// import messageData from './helpers/Data/messageData';
 import darkTheme from './components/darkTheme/darkTheme';
 
 console.error('hi');
