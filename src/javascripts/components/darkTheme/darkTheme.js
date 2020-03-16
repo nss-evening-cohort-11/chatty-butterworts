c9eeb15a44553c01d7cc6183c88ad222ac1b539c
Aaron Lain <aaron.j.lain@gmail.com>
2020-03-16 16:33:48 -0500

    here we go again

--- src/javascripts/components/darkTheme/darkTheme.js
+++ src/javascripts/components/darkTheme/darkTheme.js
@@ -1,14 +1,4 @@
-<<<<<<< HEAD
 const myFunction = () => {
   document.body.classList.toggle('dark-mode');
 };
 export default { myFunction };
-=======
-
-// const myFunction = () => {
-//   // $('body').toggleClass('.dark-mode');
-//   // document.body.classList.add('dark-mode');
-// };
-//
-// export default { myFunction };
->>>>>>> parent of a749bf1... OKAY
